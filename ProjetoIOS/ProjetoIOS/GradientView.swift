2a713a1470100bb5893fc42889f835ce21b458b3
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-11-12 10:53:15 -0300

    TRUE Initial commit

---
+++ ProjetoIOS/ProjetoIOS/GradientView.swift
@@ -0,0 +1,22 @@
+//
+//  GradientView.swift
+//  ProjetoIOS
+//
+//  Created by COTEMIG on 11/11/21.
+//
+
+import UIKit
+
+class  GradientView: UIView {
+    var topColor: UIColor = #colorLiteral(red: 0.336632967, green: 0.0837732479, blue: 0.4643940926, alpha: 1)
+    var bottomColor: UIColor = #colorLiteral(red: 0, green: 1, blue: 0.8784313725, alpha: 1)
+    
+    var startPointX: CGFloat = 0
+    var startPointY: CGFloat = 0
+    var endPointX: CGFloat = 1
+    var endPointY: CGFloat = 1
+    
+    override func layoutSubviews() {
+        let 
+    }
+}
---
+++ ProjetoIOS/ProjetoIOS/SenhaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SenhaViewController.swift
+//  ProjetoIOS
+//
+//  Created by COTEMIG on 11/11/21.
+//
+
+import UIKit
+
+class SenhaVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.Gradient(cor1: Cores.roxo, cor2: Cores.azul)
+    }
+    
+    
+}
+
+
