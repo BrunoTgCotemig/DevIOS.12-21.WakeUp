42c1650ab206623283b9f5d06f1bf0b4988b3f5e
Bruno TG <11900989@aluno.cotemig.com.br>
2021-12-08 06:49:27 -0800

    [MoreCrudActions] - First commit

---
+++ ProjetoIOS/ViewControllers/AppWorkflow/Lembretes/VC_Lembretes_EditAndDelete.swift
@@ -0,0 +1,29 @@
+//
+//  VC_Lembretes_EditAndDelete.swift
+//  ProjetoIOS
+//
+//  Created by BTG on 08/12/21.
+//
+
+import UIKit
+
+class VC_Lembretes_EditAndDelete: ViewControllerExtended {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
